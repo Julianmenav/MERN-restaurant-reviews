6e61f0f69c3725469679ee51cfe9898355da3a8b
Juli <julianmenav2@gmail.com>
2021-11-02 11:24:38 +0100

    Added express-async-errors package to handle errors inside async controllers

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require("express-async-errors");
 const cors = require("cors");
 
 const express = require("express");
@@ -6,14 +7,14 @@ const app = express();
 const mongoose = require("mongoose");
 
 const restaurants = require("./routes/restaurants");
-const errorHandler = require('./middleware/error-handler')
+const errorHandler = require("./middleware/error-handler");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/restaurants", restaurants);
 app.use("*", (req, res) => res.status(404).json({ error: "route not found" }));
-app.use(errorHandler)
+app.use(errorHandler);
 
 const mongoUri = process.env.MONGO_URI;
 const port = process.env.PORT || 3000;
